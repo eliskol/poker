f682f5eaecc25778d03e2e511649fd09cc19eef8
Elias <75465281+eliskol@users.noreply.github.com>
2021-11-12 16:44:33 +0000

    made Deck object, shuffle function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const Card = require('./objects/card.js')
+const Deck = require('./objects/deck.js')
+const shuffle = require('./utils/shuffle.js')
 
-const JackOfHearts = new Card("heart", "11");
-console.log(JackOfHearts.symbolicValue);
--- objects/deck.js
+++ objects/deck.js
@@ -1,11 +1,9 @@
 const Card = require('./card.js')
-let card_list = []
+let Deck = []
 
 for (i = 1; i <= 52; i++) {
     newCard = new Card((i - 1) % 4, (i % 13) + 1);
-    card_list.push(newCard);
-    // console.log(newCard.symbolicValue, newCard.suit);
+    Deck.push(newCard);
 }
 
-console.log(card_list.filter((card) => card.suit == "heart"));
-console.log(card_list.filter((card) => card.suit == "club").length);
+module.exports = Deck;
